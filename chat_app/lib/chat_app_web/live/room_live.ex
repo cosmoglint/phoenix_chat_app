8fb82e08ebdaf830a3584cde23c7dc00b9b96424
ramprakash <ram.prakash@qure.ai>
2021-08-08 01:28:20 +0530

    ADD:  presence of users

--- chat_app/lib/chat_app_web/live/room_live.ex
+++ chat_app/lib/chat_app_web/live/room_live.ex
@@ -6,7 +6,10 @@ defmodule ChatAppWeb.RoomLive do
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
     username = MnemonicSlugs.generate_slug(2)
-    if connected?(socket), do: ChatAppWeb.Endpoint.subscribe(topic)
+    if connected?(socket) do
+      ChatAppWeb.Endpoint.subscribe(topic)
+      ChatAppWeb.Presence.track(self(), topic, username,  %{})
+    end
     Logger.info(room_id)
     {:ok, assign(socket, room_id: room_id, username: username,topic: topic, message: "", messages: [%{uuid: UUID.uuid4(), content: "#{username} joined the chat", username: "system"}], temporary_assigns: [messages: []]) }
   end
@@ -30,5 +33,22 @@ defmodule ChatAppWeb.RoomLive do
   def handle_info(%{event: "new-message", "payload": message}, socket) do
     Logger.info(message: message)
     {:noreply, assign(socket, messages: [message])}
+  end
+
+  @imptl true
+  def handle_info(%{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
+    Logger.info(joins: joins, leaves: leaves)
+    join_message = joins
+      |> Map.keys()
+      |> Enum.map(fn  username ->
+        %{uuid: UUID.uuid4(), content: "#{username} joined", type: :system}
+      end)
+
+    leave_message = leaves
+      |> Map.keys()
+      |> Enum.map(fn  username ->
+        %{uuid: UUID.uuid4(), content: "#{username} left", type: :system}
+      end)
+    {:noreply, socket}
   end
 end
